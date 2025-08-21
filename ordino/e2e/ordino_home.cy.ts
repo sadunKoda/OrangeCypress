e211f1205fa2d9f57e055ce51196d4de71d594de
sadunKoda <msadunkodagoda@gmail.com>
2025-08-21 11:58:41 +0530

    test2

--- ordino/e2e/ordino_home.cy.ts
+++ ordino/e2e/ordino_home.cy.ts
@@ -25,5 +25,272 @@ ordinoSuite(
         .step_searchOption("Leave")
         .step_profileOption("Logout");
     });
+
+    //-----------------
+
+    it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leave")
+        .step_profileOption("Logout");
+    });
+
+
+    it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
+      loginPage
+        .visitUrl()
+        .step_enterUsername("Admin")
+        .step_enterPassword("admin123")
+        .step_clickLogin()
+        .step_searchOption("Leaveoo")
+        .step_profileOption("Logout");
+    });
+
   }
 );
